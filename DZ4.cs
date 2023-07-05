b6432c853d7d362a491f18d8418a7c5ad8633f8a
Alena <magii.alena@yandex.ru>
2023-07-05 19:35:38 +0300

    Решение задачи 27

--- DZ4.cs
+++ DZ4.cs
@@ -1,13 +1,25 @@
 ﻿// 25. Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B
-Console.Write("Введите возводимое в степень число: ");
-int foundation = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите степень: ");
-int power = Convert.ToInt32(Console.ReadLine());
-int result = 1;
-for (int i = 1; i <= power; i++)
-{
-    result = result*foundation;
-}
-Console.WriteLine($"Число {foundation} в степени {power} = {result}");
+// Console.Write("Введите возводимое в степень число: ");
+// int foundation = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите степень: ");
+// int power = Convert.ToInt32(Console.ReadLine());
+// int result = 1;
+// for (int i = 1; i <= power; i++)
+// {
+//     result = result*foundation;
+// }
+// Console.WriteLine($"Число {foundation} в степени {power} = {result}");
+
 // 27. Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе
+Console.Write("Введите число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+int copyNumber = userNumber;
+int sumDigital = 0;
+while (userNumber > 0)
+{
+    sumDigital = sumDigital + (userNumber % 10);
+    userNumber /= 10;
+}
+Console.WriteLine($"Сумма цифр в числе {copyNumber} равна {sumDigital}");
+
 // 29. Напишите программу, которая задаёт массив из 8 элементов в диапазоне от 10 до 1000 и выводит их на экран. Или массив из 8 элементов вводится с консоли (каждый элемент вводит человек)
